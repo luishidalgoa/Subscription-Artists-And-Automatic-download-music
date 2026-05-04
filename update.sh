b1c0cd9f0100dfd5d9cd174c9bafb8b597c4a99c
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2026-05-04 12:56:32 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Nombre del servicio/contenedor de tu proyecto
+CONTAINER_NAME="yt_subs"
+
+echo "🚀 Deteniendo contenedores..."
+docker compose down
+
+echo "📦 Actualizando repositorio..."
+git pull
+
+echo "🟢 Levantando contenedores en background..."
+docker compose up -d
+
+echo "💻 Conectando al contenedor $CONTAINER_NAME..."
+docker exec -it $CONTAINER_NAME /bin/bash
